3effa66a38073314d05f8050137dbb00d1bfa17a
Bash Ash <bash@hubspot.com>
2013-04-11 20:46:20 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,64 @@
+# TweetSweep
+
+## What is TweetSweep?
+
+TweetSweep is a lightweight command-line tool that lets you sweep any twitter handle for desired strings. It can optionally avoid undesirable strings or output to a .csv
+
+## Usage
+
+### Parameters
+
+* `-u, --usernames` 
+    * A comma-delimited string of twitter handles to search
+    * `-u bryan_ash,some_other_handle`
+    * Required
+
+* `-m, --matches`
+    * A comma-delimited string of the keywords that a tweet should include
+    * `-i P90X,HubSpot`
+    * Required
+
+* `-e, --excludes`
+    * A comma-delimited string of the keywords that shouldn't be present in a tweet
+    * `-e instagram`
+    * Not Required
+
+* `-o, --output`
+    * An optional CSV file to output the results to. If this isn't included, the results are printed to the console.
+    * `-o tweets.csv`
+    * Not required
+
+* `-a, --any`
+    * Match any of the provided keywords.
+    * `-a`
+    * Not required
+
+* `-i, --insensitive`
+    * Make the search case-insensitive
+    * `-i`
+    * Not required
+
+## Sample Output
+
+`python tweetsweep.py -u bryan_ash -m p90x,hubspot -i -a`
+
+Handle: bryan_ash
+
+Number of tweets: 147
+
+Get ready, get set, #getatme with @hubspotdev http://t.co/WGhT8qCJBe
+
+..........
+
+Foam Roller acquired. Get ready @P90X2, I'm coming for ya! #bestshapeofmylife
+
+Ohmygoshohmygosh #bringit @P90X2 http://t.co/BjUkKpcd
+
+## Disclaimer
+
+Only tested on Mac OS X.
+Pull requests are welcome!
+
+## License
+Copyright (c) 2013 Bryan (Bash) Ash
+Licensed under the MIT license.
---
+++ tweetsweep.py
@@ -0,0 +1,139 @@
+import json
+import urllib2
+import csv
+import argparse
+import operator
+
+
+def look_for_tweets(handles, matches, excludes, **options):
+    if options['csv_to_output']:
+        with open(options['csv_to_output'], 'wb') as file:
+            get_tweets_for_users(handles, matches, excludes, file, options)
+    else:
+        get_tweets_for_users(handles, matches, excludes, None, options)
+
+
+def get_tweets_for_users(handles, matches, excludes, file, options):
+    for handle in handles:
+        tweets = get_tweets_for_user(handle, matches, excludes, options)
+
+        if file:
+            print_tweets_to_csv(file, handle, tweets)
+        else:
+            print_tweets_to_console(handle, tweets)
+
+
+def get_tweets_for_user(handle, matches, excludes, options):
+    relevant_tweets = []
+    max_id = 0
+    while True:
+        tweets = get_page_of_tweets(handle, max_id)
+        if not len(tweets):
+            break
+
+        relevant_tweets.extend([tweet for tweet in tweets if is_relevant_tweet(tweet, matches, excludes, options)])
+        max_id = tweets[-1]['id'] - 1
+
+    return relevant_tweets
+
+
+def get_page_of_tweets(handle, max_id):
+    url = "http://api.twitter.com/1/statuses/user_timeline/%s.json?count=200&include_entities=true" % handle
+    if max_id:
+        url += "&max_id=%s" % max_id
+
+    tweets = json.load(urllib2.urlopen(url))
+    return tweets
+
+
+def is_relevant_tweet(tweet, matches, excludes, options):
+    text = tweet['text']
+    if options['case_insensitive']:
+        text = text.lower()
+
+    matches_cache = dict((item, False) for item in matches)
+    excludes_cache = dict((item, False) for item in excludes)
+
+    for bad in excludes:
+        if bad in text:
+            excludes_cache[bad] = True
+
+    found_exclude = reduce_dict_cache(excludes_cache, options)
+    if found_exclude:
+        return False
+
+    for match in matches:
+        if match in text:
+            matches_cache[match] = True
+
+    found_match = reduce_dict_cache(matches_cache, options)
+    return found_match
+
+
+def print_tweets_to_console(handle, tweets):
+    print "Handle: %s" % handle
+    print "Number of tweets: %i" % len(tweets)
+
+    for tweet in tweets:
+        print "%s" % tweet['text']
+
+    print ""
+
+
+def print_tweets_to_csv(file, handle, tweets):
+    tweet_row = ['Tweets']
+    tweet_row.extend([tweet['text'].encode('utf8') for tweet in tweets])
+
+    file_writer = csv.writer(file)
+    file_writer.writerow(['Handle', handle])
+    file_writer.writerow(['Number of tweets', len(tweets)])
+    file_writer.writerow(tweet_row)
+    file_writer.writerow([])
+
+
+def get_arguments():
+    parser = argparse.ArgumentParser(description='Get tweets for users that contain any of the given strings')
+    parser.add_argument('-u', '--usernames', help='A comma-delimited string of the twitter handles to search', required=True)
+    parser.add_argument('-m', '--matches', help='A comma-delimited string of the keywords that it may include', required=True)
+    parser.add_argument('-e', '--excludes', help='A comma-delimited string of the keywords that it can\'t include', required=False)
+    parser.add_argument('-o', '--output', help='An optional CSV file to output the results to', required=False)
+    parser.add_argument('-a', '--any', help='Match any of the provided parameters', required=False, action='store_true')
+    parser.add_argument('-i', '--insensitive', help='Make the search insensitive', required=False, action='store_true')
+    arguments = vars(parser.parse_args())
+
+    return {
+        'handles': arguments['usernames'].split(','),
+        'matches': format_list_arg('matches', arguments),
+        'excludes': format_list_arg('excludes', arguments),
+        'csv_to_output': arguments['output'],
+        'single_match': arguments['any'],
+        'case_insensitive': arguments['insensitive']
+    }
+
+
+def format_list_arg(key, options):
+    list_string = options[key]
+    if not list_string:
+        return []
+
+    list = list_string.split(',')
+    if options['insensitive']:
+        return [item.lower() for item in list]
+    else:
+        return list
+
+
+def reduce_dict_cache(cache, options):
+    list = cache.values()
+    if not len(list):
+        return False
+
+    if options['single_match']:
+        return reduce(lambda x, y: x or y, list)
+    else:
+        return reduce(lambda x, y: x and y, list)
+
+
+if __name__ == "__main__":
+    arguments = get_arguments()
+    look_for_tweets(**arguments)
